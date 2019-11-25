3dde992340d29ef94c70b9644aeda90e92da8b14
ast_mpozhydaiev <m.pozhydaiev@astoundcommerce.com>
2019-11-25 10:31:18 +0200

    add test user and test db to make sample work

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,13 +1,13 @@
-const jwt = require("jsonwebtoken");
-const expressJwt = require("express-jwt");
-const isProd = process.env.NODE_ENV === "production";
-if (!isProd) require("dotenv").config({ path: "server/env/variables.env" });
-const User = require("../models/User");
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
+const isProd = process.env.NODE_ENV === 'production';
+if (!isProd) require('dotenv').config({ path: 'server/env/variables.test.env' });
+const User = require('../models/User');
 
 exports.signup = (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then(user => {
-      if (user) return res.status(403).json({ error: "Email is taken" });
+      if (user) return res.status(403).json({ error: 'Email is taken' });
       User.create(req.body)
         .then(user => {
           req.body.user = user;
@@ -21,11 +21,11 @@ exports.signup = (req, res, next) => {
 exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then(user => {
-      if (!user) return res.status(401).json({ error: "Authentication error" });
+      if (!user) return res.status(401).json({ error: 'Authentication error' });
 
       // check password - model method
       if (!user.authenticate(req.body.password)) {
-        return res.status(401).json({ error: "Authentication error" });
+        return res.status(401).json({ error: 'Authentication error' });
       }
 
       req.body.user = user;
@@ -41,18 +41,18 @@ exports.returnToken = (req, res) => {
   const token = jwt.sign({ name, email, _id }, process.env.SECRET_OR_KEY);
 
   // attach cookie
-  res.cookie("social-base", token, { expire: new Date() + 3600 });
+  res.cookie('social-base', token, { expire: new Date() + 3600 });
 
   // return data
   return res.json({ token, user: { _id, name, email } });
 };
 
 exports.signout = (req, res) => {
-  res.clearCookie("social-base");
+  res.clearCookie('social-base');
   res.end();
 };
 
 exports.isAuth = expressJwt({
   secret: process.env.SECRET_OR_KEY,
-  userProperty: "auth"
+  userProperty: 'auth'
 });
--- server/index.js
+++ server/index.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const log = require("morgan");
-const mongoose = require("mongoose");
-const routes = require("./routes");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const log = require('morgan');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 
 // settings
 const app = express();
-app.use(log("dev"));
+app.use(log('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === 'production';
 if (!isProd) {
-  require("dotenv").config({ path: "server/env/variables.env" });
-  const cors = require("cors");
+  require('dotenv').config({ path: 'server/env/variables.test.env' });
+  const cors = require('cors');
   app.use(cors());
 }
 
@@ -22,19 +22,19 @@ if (!isProd) {
 const dbOptions = { useNewUrlParser: true, useFindAndModify: false };
 mongoose
   .connect(process.env.DB_URL, dbOptions)
-  .then(() => console.log("db connected"))
+  .then(() => console.log('db connected'))
   .catch(err => console.log(err.message));
 
 // include routes
-app.use("/", routes);
+app.use('/', routes);
 
 // error handler
 app.use(function(err, req, res, next) {
-  if (err.name === "UnauthorizedError") {
-    res.status(401).send("Authentication error");
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).send('Authentication error');
   }
 });
 
 // launch server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server run"));
+app.listen(PORT, () => console.log('Server run'));
